95e556d3b9e8d124dea443b26a5188d83f3e3a7c
Ignatius Ukwuoma <ignatius.ukwuoma@andela.com>
2017-05-30 17:37:19 +0100

    [Feature #145841509] Create Paystack payment method folder and files

---
+++ imports/plugins/included/paystack-paymentmethod/register.js
@@ -0,0 +1,39 @@
+/* eslint camelcase: 0 */
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "PaystackPayment",
+  name: "paystack-paymentmethod",
+  icon: "fa fa-credit-card-alt",
+  autoEnable: true,
+  settings: {
+    mode: false,
+    apiKey: ""
+  },
+  registry: [
+    // Dashboard card
+    {
+      provides: "dashboard",
+      label: "Paystack Payment Provider",
+      description: "Paystack payment provider",
+      icon: "fa fa-credit-card-alt",
+      priority: 3,
+      container: "paymentMethod"
+    },
+
+    // Settings panel
+    {
+      label: "Paystack Payment Settings", // this key (minus spaces) is used for translations
+      route: "/dashboard/paystack",
+      provides: "settings",
+      container: "dashboard",
+      template: "paystackSettings"
+    },
+
+    // Payment form for checkout
+    {
+      template: "paystackPaymentForm",
+      provides: "paymentMethod"
+    }
+  ]
+});
